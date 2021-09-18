243e48004e009aec6546b3931119f3a1cdb37340
JanaJankovic <jana.jankovic@student.um.si>
2021-09-18 16:52:59 +0200

    ini commit

---
+++ app/src/main/java/pws/monitoring/feri/MainActivity.java
@@ -0,0 +1,14 @@
+package pws.monitoring.feri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
