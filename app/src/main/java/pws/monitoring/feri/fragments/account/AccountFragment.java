479cda35275a7da7dbd53184bede2b5a5e6b97c7
JanaJankovic <jana.jankovic@student.um.si>
2021-09-27 20:13:30 +0200

    setting up placeholder design for testing

--- app/src/main/java/pws/monitoring/feri/fragments/account/AccountFragment.java
+++ app/src/main/java/pws/monitoring/feri/fragments/account/AccountFragment.java
@@ -27,13 +27,6 @@ public class AccountFragment extends Fragment {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
